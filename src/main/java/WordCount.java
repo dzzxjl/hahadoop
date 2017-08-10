edbc37c67262eb4661ac887dd48bbca5a4ed62de
dzzxjl <dzzxjl@126.com>
2017-08-10 16:50:31 +0800

    理解map&reduce

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -16,19 +16,25 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WordCount {
 
+    /*
+    Mapper类
+     */
     public static class TokenizerMapper
             extends Mapper<Object, Text, Text, IntWritable> {
-
+        // final关键字定义1？？？
         private final static IntWritable one = new IntWritable(1);
+
         private Text word = new Text();
 
         //map函数
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
             StringTokenizer itr = new StringTokenizer(value.toString());
+            // 按行读取？
             while (itr.hasMoreTokens()) {
                 word.set(itr.nextToken());
                 context.write(word, one);
+                System.out.println(word);
             }
         }
     }
@@ -52,14 +58,22 @@ public class WordCount {
     }
 
     public static void main(String[] args) throws Exception {
+        // 加载配置
         Configuration conf = new Configuration();
+        // 使用单例模式创建一个名为word count的job
         Job job = Job.getInstance(conf, "word count");
+        // 加载本类
         job.setJarByClass(WordCount.class);
+        //  加载Map模型
         job.setMapperClass(TokenizerMapper.class);
+        // 加载Reduce模型
         job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
+        // 设置输出类
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+
+
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
